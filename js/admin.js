ab9ae9e10d942b4ac51cbb98d046c829c0b4e604
budibungalayar <budibungalayar@gmail.com>
2025-10-17 04:13:14 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -282,7 +282,7 @@ async function renderOrdersTable() {
     const orders = loadOrders();
     let filteredOrders = orders;
     
-    if (currentFilter !== 'allOrders') {
+    if (currentFilter !== 'all') {
         filteredOrders = allOrders.filter(o => o.status === currentFilter);
     }
     
@@ -678,6 +678,7 @@ window.exportOrders = exportOrders;
 window.clearAllData = clearAllData;
 window.refreshData = refreshData;
 window.logout = logout;
+
 
 
 
